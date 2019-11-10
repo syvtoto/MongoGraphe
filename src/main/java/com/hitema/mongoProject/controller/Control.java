88abfb5ec86eacc41568928d066b4a6eef1c53fe
Thomas <syvarris.toto@gmail.com>
2019-11-10 22:46:21 +0100

    Finish, pas d'update possible

--- src/main/java/com/hitema/mongoProject/controller/Control.java
+++ src/main/java/com/hitema/mongoProject/controller/Control.java
@@ -3,6 +3,8 @@ package com.hitema.mongoProject.controller;
 import java.awt.List;
 import java.util.ArrayList;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/hitema/mongoProject/dao/ConnexionRepo.java
+++ src/main/java/com/hitema/mongoProject/dao/ConnexionRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.hitema.mongoProject.model.Connexion;
 
 public interface ConnexionRepo extends MongoRepository<Connexion, String>{
-
+	
 }
