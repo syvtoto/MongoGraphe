c422d1766af82ee9a7cb27b060142c82d7327672
Thomas <syvarris.toto@gmail.com>
2019-11-11 00:18:57 +0100

    End of project

--- src/main/java/com/hitema/mongoProject/controller/Control.java
+++ src/main/java/com/hitema/mongoProject/controller/Control.java
@@ -32,7 +32,7 @@ public class Control {
 			lInteg.add(l);
 		}
 		model.addAttribute("pts",lInteg);		
-		model.addAttribute("graphe", algoService.generateGraphe(nbPoint));
+		model.addAttribute("graphe", dataService.getGraphe());
 		model.addAttribute("start", start);
 		model.addAttribute("end", end);
 		model.addAttribute("nbPoint", nbPoint);
@@ -42,47 +42,10 @@ public class Control {
 	@Autowired
     private DataService dataService;
 	
-	
-	@GetMapping("/graphe")
-	public String createAndGetGraphe(Model model) {
-		int tailleGraphe = 10;
-		List<Integer> lInteg = new ArrayList<Integer>();
-		for(int l=0;l<tailleGraphe;l++) {
-			lInteg.add(l);
-		}
-		model.addAttribute("pts",lInteg);
-		model.addAttribute("graphe", algoService.generateGraphe(tailleGraphe));
-		return "graphe";
-	}
-	
-	@GetMapping("/addStation")
-	public String createAndAddStation(Model model) {
-		dataService.addStation("FirstStation", "A", new StationLigne("A", "Mine", null, null));
-		model.addAttribute("textToDisplay","station created");
-		return "default";
-	}
-	
-	@GetMapping("/createNsaveGraphe")
+	@GetMapping("/newGraphe")
 	public String createNsaveGraphe(Model model) {
 		int[][] graphe = algoService.generateGraphe(10);
 		dataService.saveGraphe(graphe, algoService.generateListStation(graphe.length));
-		model.addAttribute("textToDisplay","graphe saved to mongo");
-		return "default";
-	}
-	
-	@GetMapping("/getAllStations")
-	public String getAllStations(Model model) {
-		List<Station> allStations = dataService.getAllStations();
-		System.out.println(allStations.toString());
-		model.addAttribute("textToDisplay","getting all stations");
-		return "default";
-	}
-	
-	@GetMapping("/getGraphe")
-	public String getGraphe(Model model) {
-		int[][] graphe = dataService.getGraphe();
-		System.out.println(graphe.toString());
-		model.addAttribute("textToDisplay","getting graphe");
-		return "default";
+		return "redirect:/index";
 	}
 }
