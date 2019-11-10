bba2cd574ebcb9c28a0b97a82ecda95fe3a73c8b
laarogue <mikhael.partouche@gmail.com>
2019-11-10 22:17:41 +0100

    add graph

--- src/main/java/com/hitema/mongoProject/controller/Control.java
+++ src/main/java/com/hitema/mongoProject/controller/Control.java
@@ -13,17 +13,24 @@ import com.hitema.mongoProject.service.AlgoService;
 @Controller
 public class Control {
 
+	@Autowired
+	private AlgoService algoService;
+	
 	@GetMapping("/index")
 	public String index(Model model,
 			@RequestParam(name="start", required = false, defaultValue = "start")String start,
 			@RequestParam(name="end", required = false, defaultValue = "end")String end) {
+		int tailleGraphe = 10;
+		ArrayList<Integer> lInteg = new ArrayList<Integer>();
+		for(int l=0;l<tailleGraphe;l++) {
+			lInteg.add(l);
+		}
+		model.addAttribute("pts",lInteg);
+		model.addAttribute("graphe", algoService.generateGraphe(tailleGraphe));
 		model.addAttribute("start", start);
 		model.addAttribute("end", end);
 		return "index";
 	}
-
-	@Autowired
-	private AlgoService algoService;
 
 	@GetMapping("/graphe")
 	public String createAndGetGraphe(Model model) {
