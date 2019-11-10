0464c66cf7cd5b0aa92499b088e8cd1d986a54b0
laarogue <mikhael.partouche@gmail.com>
2019-11-10 14:48:09 +0100

    add index controler

--- src/main/java/com/hitema/mongoProject/controller/Control.java
+++ src/main/java/com/hitema/mongoProject/controller/Control.java
@@ -1,8 +1,19 @@
 package com.hitema.mongoProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/index")
 public class Control {
+
+    @GetMapping("/index")
+    public String index(@RequestParam(name="name", required = false, defaultValue = "World")String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
 	
 }
